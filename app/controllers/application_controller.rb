c889aaceb5550c4608745cc4b31e6a67a712336e
lixu19941116 <lixu19941116@foxmail.com>
2015-02-08 23:27:49 +0800

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :page_identifier
+
+  def page_identifier
+    "#{self.class.to_s.downcase.gsub('::','-').gsub('controller','')} #{action_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+
+  get 'contact' => 'static_pages#contact'
 end
