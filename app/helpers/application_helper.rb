42e0873b422eb686a77e6966fb0ac7622b7d96fe
lixu19941116 <lixu19941116@foxmail.com>
2015-02-08 20:43:07 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #根据所在的页面返回完整的标题
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
