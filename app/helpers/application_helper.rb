0240c3d1a6f3a6a82764cdd385aae5e81a6e6bff
lixu19941116 <lixu19941116@foxmail.com>
2015-07-01 15:20:01 +0800

    refactor views/static_pages/about contact help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   #根据所在的页面返回完整的标题
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "VVeib0"
     if page_title.empty?
       base_title
     else
