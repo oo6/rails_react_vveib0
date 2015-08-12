1ce8c1232cf913b359798c627f7b344aed6e54a1
lixu19941116 <lixu19941116@foxmail.com>
2015-08-12 18:25:31 +0800

    modify model like default scope

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,7 @@ class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject, polymorphic: true
 
+  default_scope -> { order(created_at: :desc) }
+
   validates :user_id, :subject, presence: true
 end
