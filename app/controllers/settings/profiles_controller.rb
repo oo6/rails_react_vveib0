cd90ef706f40ffa4e485a8c765b49490c4d38609
lixu19941116 <lixu19941116@foxmail.com>
2015-08-24 21:27:12 +0800

    finish settings profile

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -1,4 +1,18 @@
 class Settings::ProfilesController < Settings::ApplicationController
   def show
   end
+
+  def update
+    if @user.update_attributes setting_params
+      flash[:success] = '成功'
+      redirect_to settings_profile_url
+    else
+      render :show
+    end
+  end
+
+  private
+    def setting_params
+      params.require(:user).permit(:name, :email)
+    end
 end
