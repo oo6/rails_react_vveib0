2c3b4e57987430968ed96bb0ff84cd3324ea1f76
lixu19941116 <lixu19941116@foxmail.com>
2015-09-09 09:08:49 +0800

    API v1: get user microposts

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -4,15 +4,20 @@ module V1
       namespace ':name' do
         before do
           @user = User.find_by(name: params[:name])
+          correct_find!(@user)
         end
 
         desc "获取用户详细资料"
         get '', serializer: UserDetailSerializer, root: 'user' do
-          if @user
-            render @user
-          else
-            error!('404 Not Found', 404)
-          end
+          render @user
+        end
+
+        desc "获取某个用户发布的微博"
+        params do
+          optional :page, type: Integer, default: 1
+        end
+        get :microposts, each_serializer: MicropostSerializer, root: 'microposts' do
+          render @user.microposts.paginate(page: params[:page])
         end
       end
     end
